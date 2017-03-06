085b81113fe94393c90e076153f5db153c27992f
RPW <rpwalsh@sfu.ca>
2017-03-05 16:18:52 -0800

    Summary: Updated input_PerXNeuY.m and ToySolver.m to rely on initial data 	 not exact data. Documentation has been updated accordingly
    input_PerXNeuY.m: - Now specifies exact solution along with initial data;
    		    completely compatable with the updates to ToySolver.m.
    		  - Documentation has been updated to reflect new
    		    functionality
    
    ToySolver.m: - Now uses initial data definitions from input file instead
    	       of exact. No need to define exact information anymore for
    	       this function.
    	     - Convergence study for P no longer possible. We are not
    	       correcting for the unknown constant.
    	     - Documentation updated to reflect new functionality.
    	     - Visualization has been updated to use subplots for all
    	       variables.
    
    WaveTank.m: - corrected function call to ToySolver.m

--- ToySolver.m
+++ ToySolver.m
@@ -1,16 +1,32 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%This solver uses the split-step scheme 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This solver approximates solutions to the toy gravity wave problem
+% described in README. 
+% inputs:
+%        input = input file for problem specificiation: 
+%                 - uInit(x,z) = initial data for u
+%                 - wInit(x,z) = initial data for w
+%                 - rhoInit(x,z,t) = initial data for rho (time-dependent
+%                                    to maintain conv study functionality)
+%                 - PBCT(x,t) = Top Neumann BC for pressure
+%                 - PBCB(x,t) = Top Neumann BC for pressure
+%        Lx,Lz = x and z domain boundaries respectivly
+%        m,n = number of points in x and y respectivly
+%        tFinal = final computing time
+%        dt = time-step
+%        vis = boolian visual parameter
+% outputs:
+%         x,z = x and z mesh coordinates (meshgrid format)
+%         dx,dz = mesh spacing in x and y
+%         time = final computing time (could be different from tFinal)
+%         uNew,wNew = u and w solutions at t = time
+%         P = P solution at t = time-dt/2
+%         rho_nph = rho solution at t = time+dt/2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [x,z,dx,dz,time,uNew,wNew,P,rho_nph] = ToySolver(input,Lx,Lz,m,n,tFinal,dt,vis)
 
 %% Define exact solution through input file
-%input_PerXNeuY2;
 input()
-
-%% Define initial conditions
-u0 = @(x,z) uExact(x,z,0);
-w0 = @(x,z) wExact(x,z,0);
 
 %% Create spatial computing mesh
 dx = Lx/m;% corrected for periodic grid
@@ -21,113 +37,71 @@ z = 0-dz/2:dz:Lz+dz/2;
 xIn = x(2:end-1,:);
 zIn = z(2:end-1,:);
 
-%% loop for temporal convergence
-%for i = 1:4
+%% Create temporal computing mesh
+N = round(tFinal/dt);
 
-    %% Initial condition for velocity
-    uOld = u0(xIn,zIn);
-    wOld = w0(xIn,zIn);
+%% Initial condition for velocity
+uOld = uInit(xIn,zIn);
+wOld = wInit(xIn,zIn);
+
+%% Obtain rho_n+1/2 (use rho(0) when no exact solution)
+rho_nph = rhoInit(xIn,zIn,dt/2);
+
+%% Begin time-stepping
+for n=1:N
     
-    %% Create temporal computing mesh
-    N = round(tFinal/dt);
+    time = n*dt;
     
-    %% Obtain rho_n+1/2 (use rho(0) when no exact solution)
-    rho_nph = rhoExact(xIn,zIn,dt/2);
+    %% Compute drho/dz at n+1/2
+    %dzrho_nph = ([rho_nph(2:end,:); rho_nph(1,:)] - [rho_nph(end,:); ...
+    %                    rho_nph(1:end-1,:)])/dz/2;% Assumes rho is periodic
+    dzrho_nph = ([rho_nph(2:end,:); -rho_nph(end,:)] ...
+                 -  [-rho_nph(1,:); rho_nph(1:end-1,:)] )/dz/2;% Assumes rho is 0 top and bottom boundary
+    
+    %% Define top/bottom Neumann boundary conditions for pressure
+    T = @(x) PBCT(x,time-dt/2);
+    B = @(x) PBCB(x,time-dt/2);
+    
+    %% Solve for Pressure at n+1/2
+    P = Poisson(-g*dzrho_nph,x,z,B,T);
+    
+    %% Compute grad P
+    P_z = (P(3:end,:) - P(1:end-2,:))/2/dz;
+    P_x = ([P(2:end-1,2:end) P(2:end-1,1)] - [P(2:end-1,end) P(2:end-1,1:end-1)])/2/dx;
+    
+    %% Step velocity forward in time
+    uNew = uOld - dt*(P_x);
+    wNew = wOld - dt*(P_z + g*rho_nph);
+    uOld = uNew;
+    wOld = wNew;
+    
+    %% Step density forward in time
+    rho_nph = rho_nph + dt*wNew;
+    
+    if vis
+        %% Plot velocity and error
+        figure(1)
 
-    %% Begin time-stepping
-    for n=1:N
-        
-        time = n*dt;
-        
-        %% Compute drho/dz at n+1/2
-        %dzrho_nph = ([rho_nph(2:end,:); rho_nph(1,:)] - [rho_nph(end,:); ...
-        %                    rho_nph(1:end-1,:)])/dz/2;% Assumes rho is periodic
-        dzrho_nph = ([rho_nph(2:end,:); -rho_nph(end,:)] ...
-                  -  [-rho_nph(1,:); rho_nph(1:end-1,:)] )/dz/2;% Assumes rho is 0 top and bottom boundary
+        subplot(2,2,1)
+        surf(xIn,zIn,uNew,'Edgecolor','none')
+        title(['Computed u at time = ' num2str(time)])
+        view([0 90]); axis tight;
 
-        %% Define top/bottom Neumann boundary conditions for pressure
-        T = @(x) PExact_z(x,2*pi,time-dt/2);
-        B = @(x) PExact_z(x,0,time-dt/2);
+        subplot(2,2,2)
+        surf(xIn,zIn,wNew,'Edgecolor','none')
+        title(['Computed w at time = ' num2str(time)])
+        view([0 90]); axis tight;
         
-        %% Solve for Pressure at n+1/2
-        P = Poisson(-g*dzrho_nph,x,z,B,T);
+        subplot(2,2,3)
+        surf(xIn,zIn,rho_nph,'Edgecolor','none')
+        title(['Computed rho at time = ' num2str(time+dt/2)])
+        view([0 90]); axis tight;
         
-        %% Correct for unknown constant
-        PExactDisc = PExact(x,z,time-dt/2);
-        const = P(10,10) - PExactDisc(10,10);
-        P = P - const;
-        
-        %% Compute grad P
-        P_z = (P(3:end,:) - P(1:end-2,:))/2/dz;
-        P_x = ([P(2:end-1,2:end) P(2:end-1,1)] - [P(2:end-1,end) P(2:end-1,1:end-1)])/2/dx;
-        
-        %% Step velocity forward in time
-        uNew = uOld - dt*(P_x);
-        wNew = wOld - dt*(P_z + g*rho_nph);
-        uOld = uNew;
-        wOld = wNew;
-        
-        %% Step density forward in time
-        rho_nph = rho_nph + dt*wNew;
-        
-        %% Compute point-wise errors
-        uError = uNew - uExact(xIn,zIn,time);
-        wError = wNew - wExact(xIn,zIn,time);
-        rhoError = rho_nph - rhoExact(xIn,zIn,time+dt/2);
-
-        if vis
-            %% Plot velocity and error
-            figure(1)
-            surf(xIn,zIn,rho_nph,'Edgecolor','none')
-            title(['Computed rho at time = ' num2str(time+1/2)])
-            view([0 90]);
-            figure(2)
-            surf(x,z,P,'Edgecolor','none')
-            title(['Computed P at time = ' num2str(time+1/2)])
-            view([0 90]);
-            drawnow
-            pause(.1)
-        end
+        subplot(2,2,4)
+        surf(x,z,P,'Edgecolor','none')
+        title(['Computed P at time = ' num2str(time-dt/2)])
+        view([0 90]); axis tight;
+        drawnow
+        pause(.1)
     end
-    
-    %% Compute Inf-norm errors
-    %uInfError(i) = max(abs(uError(:)));
-    %wInfError(i) = max(abs(wError(:)));
-    %rhoInfError(i) = max(abs(rhoError(:)));
-    %TimeStep(i) = dt;
-
-    %disp(['Inf-norm error: ' num2str(uInfError(i))])
-
-    %end
-
-if 0
-    figure
-    plot(log(TimeStep),log(uInfError),'o')
-    pfit = polyfit(log(TimeStep),log(uInfError),1);
-    hold on
-    plot(log(TimeStep),pfit(1)*log(TimeStep) + pfit(2));
-    legend('Numerical Convergence',[num2str(pfit(1)) ' + (' num2str(pfit(2)) ')'])
-    xlabel('$\log(\Delta t)$','interpreter','latex')
-    ylabel('$\log(||u-u_h||_\infty)$','interpreter','latex')
-    title('Temporal Convergence of u')
-    
-    figure
-    plot(log(TimeStep),log(wInfError),'o')
-    pfit = polyfit(log(TimeStep),log(wInfError),1);
-    hold on
-    plot(log(TimeStep),pfit(1)*log(TimeStep) + pfit(2));
-    legend('Numerical Convergence',[num2str(pfit(1)) ' + (' num2str(pfit(2)) ')'])
-    xlabel('$\log(\Delta t)$','interpreter','latex')
-    ylabel('$\log(||w-w_h||_\infty)$','interpreter','latex')
-    title('Temporal Convergence of w')
-    
-    figure
-    plot(log(TimeStep),log(rhoInfError),'o')
-    pfit = polyfit(log(TimeStep),log(rhoInfError),1);
-    hold on
-    plot(log(TimeStep),pfit(1)*log(TimeStep) + pfit(2));
-    legend('Numerical Convergence',[num2str(pfit(1)) ' + (' num2str(pfit(2)) ')'])
-    xlabel('$\log(\Delta t)$','interpreter','latex')
-    ylabel('$\log(||\rho-\rho_h||_\infty)$','interpreter','latex')
-    title('Temporal Convergence of \rho')
 end
--- input_PerXNeuY.m
+++ input_PerXNeuY.m
@@ -1,26 +1,42 @@
-%% Input file for ToySolver2.m using periodic BCs in x and Neumann in y for
-%% the pressure inversion
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+% -Input file for ToySolver.m using periodic BCs in x and homogenous
+% Neumann in y for the pressure inversion.
+% -This input file contains both the problem set up as well as the exact
+%  solution i.e. this input file is well suited for a convergence study.
+%
+% This input file MUST specify atleast the following:
+%                 - uInit(x,z) = initial data for u
+%                 - wInit(x,z) = initial data for w
+%                 - rhoInit(x,z,t) = initial data for rho (time-dependent
+%                                    to maintain conv study functionality)
+%                 - PBCT(x,t) = Top Neumann BC for pressure
+%                 - PBCB(x,t) = Top Neumann BC for pressure
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Define exact solution
+% Define gravity and dispersion relation
 g = 9.81; omega = @(k,l) sqrt(g*k^2/(k^2+l^2));
 
+% Define derived Fourier solution as a function of wavenumbers k and l
 uTmp = @(k,l,x,z,t) 1i*l^2*omega(k,l)*exp(1i*(k*x + l*z + omega(k,l)*t));
 wTmp = @(k,l,x,z,t) -1i*k*l*omega(k,l)*exp(1i*(k*x + l*z + omega(k,l)*t));
 PTmp = @(k,l,x,z,t) 1i*k*(omega(k,l)^2-g)*exp(1i*(k*x + l*z + omega(k,l)*t));
 rhoTmp = @(k,l,x,z,t) -k*l*exp(1i*(k*x + l*z + omega(k,l)*t));
 
+% Define k and l (k and l must be even to maintain 2pi periodicity)
 k = 4; l = 4;
 
+% Define exact solution. Note that we have added l and -l solutions: this
+% generates a solution that satisfies homogenuous Neumann BC's in P and
+% homogenuous dirichlet for w and rho (start 0 stay 0)
 uExact = @(x,z,t) 2*real(uTmp(k,l,x,z,t) + uTmp(k,-l,x,z,t));
 wExact = @(x,z,t) 2*real(wTmp(k,l,x,z,t) + wTmp(k,-l,x,z,t));
 PExact = @(x,z,t) 2*real(PTmp(k,l,x,z,t) + PTmp(k,-l,x,z,t));
 rhoExact = @(x,z,t) 2*real(rhoTmp(k,l,x,z,t) + rhoTmp(k,-l,x,z,t));
-
 PExact_z = @(x,z,t) 2*real(1i*l*PTmp(k,l,x,z,t) - 1i*l*PTmp(k,-l,x,z,t));
 
-%PExact_x = @(x,z,t) -k^2*(omega^2-g)*exp(1i*(k*x + l*z + omega*t)) + ...
-%    -k^2*(omega^2-g)*exp(-1i*(k*x + l*z + omega*t));
-%PExact_z = @(x,z,t) -l*k*(omega^2-g)*exp(1i*(k*x + l*z + omega*t)) + ...
-%    -l*k*(omega^2-g)*exp(-1i*(k*x + l*z + omega*t));
-%rhoExact_z = @(x,z,t) -1i*k*l^2*exp(1i*(k*x + l*z + omega*t)) + ...
-%    1i*k*l^2*exp(-1i*(k*x + l*z + omega*t));
+% Define initial data for u,w,rho and BC's for P.
+uInit = @(x,z) uExact(x,z,0);
+wInit = @(x,z) wExact(x,z,0);
+rhoInit = @(x,z,t) rhoExact(x,z,t);
+PBCT = @(x,t) zeros(size(x)); 
+PBCB = @(x,t) zeros(size(x)); 
